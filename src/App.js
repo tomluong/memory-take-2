3e9647e7581f76b9271591e41e4a06a355101c58
Tom Luong <tomluong@live.com>
2020-09-07 22:28:26 -0400

    update logic

--- src/App.js
+++ src/App.js
@@ -3,14 +3,37 @@ import Card from "./components/card/card";
 import "./styles.css";
 
 export default function App() {
-  const wordCount = 4;
+  const wordCount = 6;
   const [words, setWords] = useState(generateWords(wordCount));
+
+  const onClicked = (wordId) => {
+    console.log(wordId);
+
+    const _words = [...words];
+
+    for (let i = 0; i < _words.length; i++) {
+      if (_words[i].id === wordId) {
+        _words[i].turned = true;
+        break;
+      }
+    }
+
+    setWords(_words);
+  };
   return (
     <div className="App">
       <h1>React Memory</h1>
       <div className="cards">
         {words.map((x) => (
-          <Card word={x.text} meaning={x.meaning} />
+          <Card
+            key={x.id}
+            id={x.id}
+            word={x.text}
+            meaning={x.meaning}
+            turned={x.turned}
+            matched={x.matched}
+            onClicked={onClicked}
+          />
         ))}
       </div>
     </div>
@@ -19,14 +42,28 @@ export default function App() {
   function generateWords(count) {
     const words = require("./words.json");
 
-    // shuffle words
-    for (let i = words.length - 1; i > 0; i--) {
-      const randomIndex = Math.floor(Math.random() * i);
-      const tempWord = words[i];
-      words[i] = words[randomIndex];
-      words[randomIndex] = tempWord;
-    }
+    shuffleArray(words);
 
-    return words.slice(0, count);
+    // select the number of words
+    const selectedWords = words.slice(0, count);
+
+    // duplicate words
+    const selectedWordsWithDuplicates = [...selectedWords, ...selectedWords];
+
+    shuffleArray(selectedWordsWithDuplicates);
+
+    return selectedWordsWithDuplicates.map((word, index) => {
+      return Object.assign({ id: index, turned: false, matched: false }, word);
+    });
+  }
+
+  function shuffleArray(array) {
+    // shuffle words
+    for (let i = array.length - 1; i > 0; i--) {
+      const randomIndex = Math.floor(Math.random() * i);
+      const temp = array[i];
+      array[i] = array[randomIndex];
+      array[randomIndex] = temp;
+    }
   }
 }
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./card.css";
 
-export default function Card({ word, meaning }) {
+export default function Card({ id, word, meaning, turned, onClicked }) {
+  const clicked = () => {
+    onClicked(id);
+  };
+
   return (
-    <div className="card">
+    <div className={turned ? "card turned" : "card"} onClick={clicked}>
       <div className="card-body">
         <div className="card-front">
           <div className="word">{word}</div>
